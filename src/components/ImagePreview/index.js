26a18e851b864e2f195c64cffe374ef0126bc28d
Александр Савельев <Savelyev@vector-best.ru>
2022-08-23 09:58:58 +0700

    PagePhoto - начал делать

---
+++ src/components/ImagePreview/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+//import PropTypes from 'prop-types';
+import style from './index.module.css'
+
+export const ImagePreview = () => {
+	return (
+		<div className={style.body}>
+			ImagePreview
+		</div>
+	)
+}
+
+/*
+ImagePreview.propTypes = {
+	count: PropTypes.number
+	}
+*/
--- src/pages/PagePhoto/index.js
+++ src/pages/PagePhoto/index.js
@@ -1,11 +1,43 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { ImagePreview } from '../../components/ImagePreview';
 //import PropTypes from 'prop-types';
 import style from './index.module.css'
+
+const fileToDataUrl = file => {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+  
+    fileReader.addEventListener('load', evt => {
+      resolve(evt.currentTarget.result);
+    });
+    
+    fileReader.addEventListener('error', evt => {
+      reject(new Error(evt.currentTarget.error));
+    });
+    
+    fileReader.readAsDataURL(file);
+  });
+}
+
+const handleSelect = async (evt) => {
+    const files = [...evt.target.files];
+    const urls = await Promise.all(files.map(o => fileToDataUrl(o)));
+    // У вас в массиве - dataUrl, можете использовать в качестве значения атрибута src тега img
+}
+
 
 export const PagePhoto = () => {
 	return (
 		<div className={style.body}>
-			PagePhoto
+			<div className={style.fileInput}>
+				<div className={style.fileText}>Click to select</div>
+				<input type="file" id="image" accept="image/*" multiple/>
+			</div>
+			<div>
+				<ImagePreview />
+				<ImagePreview />
+
+			</div>
 		</div>
 	)
 }
